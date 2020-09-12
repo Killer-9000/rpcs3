a46dd8b53549040995bed3c002f5211a87ae708b
illusion <37698908+illusion0001@users.noreply.github.com>
2020-09-12 23:48:19 +1100

    make res scale dynamic (unsafe)

--- rpcs3/Emu/system_config.h
+++ rpcs3/Emu/system_config.h
@@ -172,7 +172,7 @@ struct cfg_root : cfg::node
 		cfg::_bool debug_program_analyser{ this, "Debug Program Analyser", false };
 		cfg::_int<1, 8> consecutive_frames_to_draw{ this, "Consecutive Frames To Draw", 1, true};
 		cfg::_int<1, 8> consecutive_frames_to_skip{ this, "Consecutive Frames To Skip", 1, true};
-		cfg::_int<50, 800> resolution_scale_percent{ this, "Resolution Scale", 100 };
+		cfg::_int<50, 800> resolution_scale_percent{ this, "Resolution Scale", 100, true };
 		cfg::uint<0, 16> anisotropic_level_override{ this, "Anisotropic Filter Override", 0, true };
 		cfg::_int<-16, 16> texture_lod_bias{ this, "Texture LOD Bias Addend", 0, true };
 		cfg::_int<1, 1024> min_scalable_dimension{ this, "Minimum Scalable Dimension", 16 };
